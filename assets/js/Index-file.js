6a8a85ca11a47883b40404f530cc54c3311c51df
Brad-Hammond <brad_hammond1@sky.com>
2024-04-20 18:34:10 +0100

    Feat: Added new comments to index.js file.

--- assets/js/Index-file.js
+++ assets/js/Index-file.js
@@ -1,26 +1,24 @@
-// Variable selector
-// let howToPlayModal = document.getElementById("howToPlayButton");
-
-// Add event listener to the "How to Play" button
+// Event listener for how to play button
 document.getElementById("howToPlayButton").addEventListener("click", () => {
-  // Display the how-to-play modal
+  // Call function
   displayHowToPlayModal();
 });
-
-// Function to display the "How to Play" modal
+// Function
 function displayHowToPlayModal() {
   // Create a modal element
   const modal = document.createElement("div");
   modal.classList.add("modal");
 
-  // Content for the "How to Play" modal
+  // html content
   const howToPlayModalContent = `
         <div class="modal-content">
             <h2>How to Play</h2>
             <p>Welcome to Fast Fingers!</p>
-            <p>To play, simply type the given text in the input field.</p>
-            <p>Your goal is to type as accurately and quickly as possible.</p>
+            <p>To play, simply press the play button and start typing in the text field.</p>
+            <p>Once you start typing, your timer will start.</p>
+            <p>Your goal is to type as fast and accuratley as possible.</p>
             <p>Once you've finished typing, your scores will be displayed.</p>
+            <p>Once finished, you can press the try again button to replay with a different quote</p>
             <p>Good luck and have fun!</p>
         </div>
     `;
